63a4d9e4fa8b82331cc5ce11a94aa6758764cfe2
Zachary Seguin <zachary@zacharyseguin.ca>
2019-07-21 21:29:53 -0400

    Add test for expanding plugins in store directive
    Signed-off-by: Zachary Seguin <zachary@zacharyseguin.ca>

--- config-reloader/processors/extract_plugins_test.go
+++ config-reloader/processors/extract_plugins_test.go
@@ -163,3 +163,107 @@ func TestNothingToExpand(t *testing.T) {
 	matchDir = processed[2]
 	assert.Equal(t, "some_type", matchDir.Type())
 }
+
+func TestExpandPluginsInCopyStore(t *testing.T) {
+	pluginDef := `
+<plugin p1>
+	@type es
+	username admin
+	buffer_path /hello/world
+	buffer_size 1m
+	<buffer>
+		@type file
+		path /var/log/fluentd.*.buffer
+	</buffer>
+</plugin>
+<plugin p2>
+	@type logzio
+</plugin>
+`
+
+	es, err := fluentd.ParseString(pluginDef)
+	assert.Nil(t, err)
+
+	// prepare context as if there is a plugin already defined in kube-system
+	g := &GenerationContext{
+		Plugins: map[string]*fluentd.Directive{
+			"p1": es[0],
+			"p2": es[1],
+		},
+	}
+
+	nsConf := `
+<filter **>
+  @type grep
+</filter>
+
+<match **>
+  @type copy
+
+  <store>
+    # this is reference to the p1 plugin
+    @type p1
+
+    # param is copied
+    param1 value1
+
+    # param is overriden
+    buffer_size 5m
+  </store>
+  <store>
+    @type p2
+  </store>
+  <store>
+    @type es
+  </store>
+</match>
+
+<match **>
+  @type some_type
+</match>
+	`
+
+	fragment, err := fluentd.ParseString(nsConf)
+	assert.Nil(t, err)
+
+	ctx := &ProcessorContext{
+		GenerationContext: g,
+		Namepsace:         "unit-test",
+		DeploymentID:      "whatever",
+	}
+
+	state := &expandPluginsState{}
+	state.SetContext(ctx)
+
+	processed, err := state.Process(fragment)
+	assert.Nil(t, err)
+
+	// fmt.Printf("Processed:\n%s\n", processed)
+	matchDir := processed[1]
+	assert.Equal(t, "copy", matchDir.Type())
+
+	// check first store component
+	matchStore := matchDir.Nested[0]
+	assert.Equal(t, "es", matchStore.Type())
+
+	// param that's not overriden
+	assert.Equal(t, "/hello/world", matchStore.Param("buffer_path"))
+
+	// param that's defined at the call site
+	assert.Equal(t, "value1", matchStore.Param("param1"))
+
+	// param that's overriden
+	assert.Equal(t, "5m", matchStore.Param("buffer_size"))
+
+	// nested content is present
+	assert.Equal(t, "buffer", matchStore.Nested[0].Name)
+	assert.Equal(t, "/var/log/fluentd.*.buffer", matchStore.Nested[0].Param("path"))
+
+	// check second store
+	matchStore = matchDir.Nested[1]
+	assert.Equal(t, "logzio", matchStore.Type())
+
+	// types not found in the generation context are not touched
+	matchDir = processed[2]
+	assert.Equal(t, "some_type", matchDir.Type())
+}
