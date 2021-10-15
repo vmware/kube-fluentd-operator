489c38b3ea8e47982d4cc974f162815e34e396c7
Anton Ouzounov <aouzounov@vmware.com>
2021-10-15 11:21:55 -0400

    fix(relabel): process timeout_labels also
     - write test for timeout_labels to protect from regression
     - add timeout_labels in relabel processor logic
     - add multiple other valid fluentd directives and types for relabel logic
    
    Signed-off-by: Anton Ouzounov <aouzounov@vmware.com>

--- config-reloader/processors/relabel.go
+++ config-reloader/processors/relabel.go
@@ -15,6 +15,19 @@ type rewriteLabelsState struct {
 	BaseProcessorState
 }
 
+var validLabelDirectives = []string{"match", "store", "filter", "parse", "source"}
+var validLabelTypes = []string{"relabel", "null", "forward", "stdout", "copy", "kafka", "elasticsearch"}
+
+func contains(slice []string, item string) bool {
+	set := make(map[string]struct{}, len(slice))
+	for _, s := range slice {
+		set[s] = struct{}{}
+	}
+
+	_, ok := set[item]
+	return ok
+}
+
 func normalizeLabelName(ctx *ProcessorContext, label string) string {
 	if strings.HasPrefix(label, "@$") {
 		// cross dependency to the share.go processor
@@ -28,20 +41,34 @@ func normalizeLabelName(ctx *ProcessorContext, label string) string {
 
 func (p *rewriteLabelsState) Process(input fluentd.Fragment) (fluentd.Fragment, error) {
 	normalizeAllLabels := func(d *fluentd.Directive, ctx *ProcessorContext) error {
-		if d.Name != "match" && d.Name != "store" {
+		if !contains(validLabelDirectives, d.Name) {
 			return nil
 		}
 
-		if d.Type() != "relabel" {
+		// Process any timeout_labels since they are valid labels:
+		timeoutLabel := d.Param("timeout_label")
+		if timeoutLabel != "" {
+			if !strings.HasPrefix(timeoutLabel, "@") {
+				return fmt.Errorf("bad label name %s for timeout_label, must start with @", timeoutLabel)
+			}
+
+			d.SetParam("timeout_label", normalizeLabelName(ctx, timeoutLabel))
+		}
+
+		// Continue parsing normal @labels:
+		if !contains(validLabelTypes, d.Type()) {
 			return nil
 		}
 
 		labelName := d.Param("@label")
-		if !strings.HasPrefix(labelName, "@") {
-			return fmt.Errorf("bad label name: %s", labelName)
+		if labelName != "" {
+			if !strings.HasPrefix(labelName, "@") {
+				return fmt.Errorf("bad label name %s for @label, must start with @", labelName)
+			}
+
+			d.SetParam("@label", normalizeLabelName(ctx, labelName))
 		}
 
-		d.SetParam("@label", normalizeLabelName(ctx, labelName))
 		return nil
 	}
 
