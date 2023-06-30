eb9523754a2755b1b210ac9dc1e14347813071e2
Javier Criado Marcos <jcriadomarco@vmware.com>
2023-06-30 11:59:22 +0100

    feat: publish latest image (#402)
    Publish latest image with release
    
    Signed-off-by: jcriadomarco <jcriadomarco@vmware.com>

--- config-reloader/validate-from-dir.sh
+++ config-reloader/validate-from-dir.sh
@@ -8,19 +8,21 @@ config-reloader \
   --output-dir=${dest} \
   --templates-dir=/templates \
   --datasource=fs \
+  --exec-timeout=5 \
   --fs-dir=${DATASOURCE_DIR} \
   --fluentd-binary "/usr/local/bundle/bin/fluentd -p /fluentd/plugins"
 
 rc=0
-
+if [ -z "$(ls -A ${dest})" ]; then
+  echo -n "*** Error destination folder is empty $dest"
+  exit 1
+fi
 for st in $(find ${dest} -iname *.status); do
   ns=$(basename ${st})
   ns=${ns%%.status}
   ns=${ns#ns-}
   echo -n "*** Error for namespace '$ns': "
   cat "${st}"
-  echo
-  echo
   rc=1
 done
 
